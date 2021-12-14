36baa44ee33eef88f7c326d4edd62eb8dd25104e
Cesar Aramayo <cesar_aramayo@hotmail.es>
2021-12-14 20:45:44 -0300

    agregando items a la tabla

--- src/app/components/dashboard/usuarios/usuarios.component.ts
+++ src/app/components/dashboard/usuarios/usuarios.component.ts
@@ -1,22 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
+import { Usuario } from 'src/app/interfaces/usuario';
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+const ListUsuarios: Usuario[] = [
+  {usuario: "jperez", nombre: 'Juan', apellido: "Perez",sexo: 'Masculino'},
+  {usuario: "mgomez", nombre: 'Martin', apellido: "Gomez",sexo: 'Masculino'},
+  {usuario: "ngarcia", nombre: 'Nicolas', apellido: "Garcia",sexo: 'Masculino'},
+  {usuario: "mgomez", nombre: 'Micaela', apellido: "Gomez",sexo: 'Femenino'},
+  {usuario: "jperez", nombre: 'Juan', apellido: "Perez",sexo: 'Masculino'},
+  {usuario: "jperez", nombre: 'Juan', apellido: "Perez",sexo: 'Masculino'},
 ];
 @Component({
   selector: 'app-usuarios',
@@ -25,7 +16,7 @@ const ELEMENT_DATA: PeriodicElement[] = [
 })
 export class UsuariosComponent implements OnInit {
   displayedColumns: string[] = ['usuario', 'nombre', 'apellido', 'sexo'];
-  dataSource = ELEMENT_DATA;
+  dataSource = ListUsuarios;
 
   constructor() { }
 
