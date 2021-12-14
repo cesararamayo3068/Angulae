ff53d1a5677e9255be8f6aa459a57d69de28d091
Cesar Aramayo <cesar_aramayo@hotmail.es>
2021-12-14 20:17:01 -0300

    maquetando y dandole estilos a la tabla de usuarios

--- src/app/components/shared/shared.module.ts
+++ src/app/components/shared/shared.module.ts
@@ -14,6 +14,7 @@ import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
+import {MatTableModule} from '@angular/material/table';
 
 
 
@@ -31,7 +32,10 @@ import {MatIconModule} from '@angular/material/icon';
     MatProgressSpinnerModule,
     MatToolbarModule,
     MatIconModule,
-    HttpClientModule
+    HttpClientModule,
+    MatTableModule
+    
+    
   ],
   exports:[
     CommonModule,
@@ -44,7 +48,8 @@ import {MatIconModule} from '@angular/material/icon';
     MatProgressSpinnerModule,
     MatToolbarModule,
     MatIconModule,
-    HttpClientModule
+    HttpClientModule,
+    MatTableModule
 
   ]
 })
