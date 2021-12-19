3ff73953770bd182f14ea3fee64af0098721f2a5
Cesar Aramayo <cesar_aramayo@hotmail.es>
2021-12-19 19:19:22 -0300

    creando servicio de usuarios

---
+++ src/app/services/usuario.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { Usuario } from '../interfaces/usuario';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuarioService {
+
+  ListUsuarios: Usuario[] = [
+    {usuario: "jperez", nombre: 'Juan', apellido: "Perez",sexo: 'Masculino'},
+    {usuario: "mgomez", nombre: 'Martin', apellido: "Gomez",sexo: 'Masculino'},
+    {usuario: "ngarcia", nombre: 'Nicolas', apellido: "Garcia",sexo: 'Masculino'},
+    {usuario: "mgomez", nombre: 'Micaela', apellido: "Gomez",sexo: 'Femenino'},
+    {usuario: "jperez", nombre: 'Juan', apellido: "Perez",sexo: 'Masculino'},
+    {usuario: "jperez", nombre: 'Juan', apellido: "Perez",sexo: 'Masculino'},
+  ];
+  
+
+  constructor() { }
+
+  getUsuario(){
+    //retorna una copia del listado de Usuarios
+    return this.ListUsuarios.slice();
+  }
+}
