4b10046a143016f5a8efe04609eee3fcb583a82c
Cesar Aramayo <cesar_aramayo@hotmail.es>
2021-12-19 21:42:20 -0300

    finalizando la logica de agregar usuario

--- src/app/components/dashboard/usuarios/crear-usuario/crear-usuario.component.ts
+++ src/app/components/dashboard/usuarios/crear-usuario/crear-usuario.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 import { Usuario } from 'src/app/interfaces/usuario';
 import { UsuarioService } from 'src/app/services/usuario.service';
 
@@ -11,7 +13,10 @@ import { UsuarioService } from 'src/app/services/usuario.service';
 export class CrearUsuarioComponent implements OnInit {
   sexo:any[]=['Masculino' , 'Femenino']
   form:FormGroup;
-  constructor( private fb:FormBuilder,private _usuarioService:UsuarioService) { 
+  constructor( private fb:FormBuilder,
+              private _usuarioService:UsuarioService,
+              private router:Router,
+              private _snackBar: MatSnackBar) { 
     this.form=this.fb.group({
       usuario:['',Validators.required],
       nombre:['',Validators.required],
@@ -33,5 +38,14 @@ export class CrearUsuarioComponent implements OnInit {
  
     }
     this._usuarioService.agregarUsuario(user);
+    this.router.navigate(['/dashboard/usuarios'])
+
+    this._snackBar.open('El usuario fue agregado con exito!!' ,'',{
+      duration:1500,
+      horizontalPosition:'center',
+      verticalPosition:'bottom',
+
+    })
+
   }
 }
